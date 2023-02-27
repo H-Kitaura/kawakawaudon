2bf30d1737c04594c5b4c469d2e611be8076daf7
HiroyukiKitaura <mr.worldwide_1212@icloud.com>
2023-02-27 10:31:14 +0900

    tailwindcssで型付け

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,9 +2,34 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <div>
-      メニュー一覧
-    </div>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-12 mx-auto">
+        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div className="rounded-lg h-64 overflow-hidden">
+              <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1203x503" />
+            </div>
+            <h2 className="text-xl font-medium title-font text-gray-900 mt-5">Shooting Stars</h2>
+            <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+          </div>
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div className="rounded-lg h-64 overflow-hidden">
+              <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504" />
+            </div>
+            <h2 className="text-xl font-medium title-font text-gray-900 mt-5">The Catalyzer</h2>
+            <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+          </div>
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div className="rounded-lg h-64 overflow-hidden">
+              <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505" />
+            </div>
+            <h2 className="text-xl font-medium title-font text-gray-900 mt-5">The 400 Blows</h2>
+            <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+          </div>
+          <p className="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">メニューはこちらから</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
